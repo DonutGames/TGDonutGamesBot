e3af2f41e9d567336d6c01d1bed8eb5ef7a698b2
Donut <donut.corporation@gmail.com>
2019-03-07 23:51:54 +0300

    init

--- TGDonutGamesBot.py
+++ TGDonutGamesBot.py
@@ -1,17 +1,14 @@
 import telebot 
 import time 
 
-token = "635347981:AAHQTSVlFLY7XL0Z3X1QbVraftoyMKuXv38"
-
-bot.telebot.TeleBot(token)
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands = ['start'])
 def start_command(message):
     chat_id = message.chat.id
+    
     while True:
-        bot.send_message(chat_id, "Бля")
-        time.sleep(0.15)    
-        bot.send_message(chat_id, "Хмт")
+        bot.send_message(chat_id, "одолжи 1000 рублей) ", time.sleep(0.15))
 
 if __name__ == "__main__":
     bot.polling(none_stop = True)
